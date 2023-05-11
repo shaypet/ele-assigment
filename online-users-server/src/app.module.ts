e83e63165cff4fecf83d161dce14c3ed1135db53
shaypet <41869045+shaypet@users.noreply.github.com>
2023-05-11 13:45:41 +0300

    serve react static build and project npm start command

--- online-users-server/src/app.module.ts
+++ online-users-server/src/app.module.ts
@@ -6,10 +6,15 @@ import { OnlineUsersModule } from './online-users/online-users.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..', 'online-users-client', 'build'),
+      exclude: ['/api/(.*)'],
+    }),
     MongooseModule.forRoot(process.env.MONGODB_URI, {
       dbName: process.env.MONGODB_DBNAME,
     }),
