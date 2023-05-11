7dcfd9fbc0b1431467bef533a618b4bc0726d101
shaypet <41869045+shaypet@users.noreply.github.com>
2023-05-11 05:06:48 +0300

    add update user-online

--- online-users-server/src/online-users/online-users.controller.ts
+++ online-users-server/src/online-users/online-users.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { OnlineUsersService } from './online-users.service';
+import { UserId } from 'src/decorators/user-id.decorator';
+import { IUserId } from 'src/interfaces/user-id.interface';
+import { Request } from 'express';
+import { AuthGuard } from 'src/guards/auth.guard';
 
 @Controller('online-users')
+@UseGuards(AuthGuard)
 export class OnlineUsersController {
   constructor(private readonly onlineUsersService: OnlineUsersService) {}
+
+  @Get()
+  update(@UserId() userId: IUserId, @Req() request: Request) {
+    this.onlineUsersService.addOrUpdateEntry(userId, request);
+  }
 }
--- online-users-server/src/online-users/online-users.module.ts
+++ online-users-server/src/online-users/online-users.module.ts
@@ -3,12 +3,14 @@ import { OnlineUsersService } from './online-users.service';
 import { OnlineUsersController } from './online-users.controller';
 import { OnlineUser, OnlineUserSchema } from 'src/schemas/online-users.schema';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: OnlineUser.name, schema: OnlineUserSchema },
     ]),
+    UsersModule,
   ],
   controllers: [OnlineUsersController],
   providers: [OnlineUsersService],
